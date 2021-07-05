9ff71de12e4625edd9965220ca3d7d32709b37c0
JordanChoi <s10208161@connect.np.edu.sg>
2021-07-06 00:55:31 +0800

    Created the activities required for MenuActivity

---
+++ app/src/main/java/sg/edu/np/mad/teampk/stufftrek/LocationActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.teampk.stufftrek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
---
+++ app/src/main/java/sg/edu/np/mad/teampk/stufftrek/UnassignedItemsActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.teampk.stufftrek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UnassignedItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unassigned_items);
+    }
+}
