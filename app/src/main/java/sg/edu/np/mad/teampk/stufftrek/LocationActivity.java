4906160142f7ac8ec31336f0b9ce380c2d800ec2
JordanChoi <s10208161@connect.np.edu.sg>
2021-07-06 04:26:09 +0800

    Implemented the customized Actionbar in LocationActivity. Available for reuse in other activities.

--- app/src/main/java/sg/edu/np/mad/teampk/stufftrek/LocationActivity.java
+++ app/src/main/java/sg/edu/np/mad/teampk/stufftrek/LocationActivity.java
@@ -1,8 +1,15 @@
 package sg.edu.np.mad.teampk.stufftrek;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class LocationActivity extends AppCompatActivity {
 
@@ -10,5 +17,48 @@ public class LocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        // Receive Intent
+        Intent receiveIntent = getIntent();
+
+        // Action Bar Customization
+        ActionBar actionBar = getSupportActionBar();
+        // actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#7CCBCE")));
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_actionbar);
+        View view = getSupportActionBar().getCustomView();
+
+        // Remove the space at the start and end of the customized action bar
+        Toolbar parent =(Toolbar) view.getParent();
+        parent.setPadding(0,0,0,0); //for tab otherwise give space in tab
+        parent.setContentInsetsAbsolute(0,0);
+
+        // Widgets in the ActionBar
+        ImageButton backBtn = view.findViewById(R.id.backBtn);
+        ImageButton rightBtn = view.findViewById(R.id.rightBtn);
+        TextView title = view.findViewById(R.id.abTitleTV);
+
+        // Set Title in the Actionbar
+        title.setText("Manage Location");
+
+        // rightBtn.setImageResource(R.drawable.ic_more); // for future usage
+        // OnClickListener for buttons in the actionbar
+        backBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View V)
+            {
+                finish();
+            }
+        });
+
+        rightBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View V)
+            {
+                /* TO REMOVE THE COMMENT WHEN CreateLocationActivity is created.
+                Intent createLocationActivity = new Intent(LocationActivity.this, CreateLocationActivity.class);
+                startActivity(createLocationActivity);
+                */
+            }
+        });
     }
 }
