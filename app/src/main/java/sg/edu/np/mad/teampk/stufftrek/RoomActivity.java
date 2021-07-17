d222ade79e663a2a1e7892a13fd29a0caafa5c29
JordanChoi <s10208161@connect.np.edu.sg>
2021-07-17 21:02:32 +0800

    Implemented the RV for items in RoomActivity - some issues with DB query as it is not retrieving any items within the room.

--- app/src/main/java/sg/edu/np/mad/teampk/stufftrek/RoomActivity.java
+++ app/src/main/java/sg/edu/np/mad/teampk/stufftrek/RoomActivity.java
@@ -1,11 +1,19 @@
 package sg.edu.np.mad.teampk.stufftrek;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoomActivity extends ActionBarActivity {
+    TextView itemsTitle;
+    TextView containersTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +29,32 @@ public class RoomActivity extends ActionBarActivity {
         ActionBarActivity.abTitle.setText(roomName);
         // Change the right button of the action bar.
         rightBtn.setImageResource(R.drawable.ic_more); // for future usage
+
+        // Respective texts within the activity
+        itemsTitle = findViewById(R.id.itemsTitleTV);
+        containersTitle = findViewById(R.id.containerTitleTV);
+        itemsTitle.setText("Items");
+        containersTitle.setText("Containers");
+
+        // Construct DBHandler to retrieve DB information.
+        DBHandler db = new DBHandler(this, null, null, 1);
+
+        // Containers RecyclerView
+
+        // Items RecyclerView
+
+        // Call DBHandler method to retrieve all items within the room
+        ArrayList<Item> roomItemsList = db.GetAllItemFromRoom(roomId);
+
+        RecyclerView rv = findViewById(R.id.itemsRV);
+        ItemsWithPathAdapter itemsAdapter = new ItemsWithPathAdapter(this, roomItemsList);
+        LinearLayoutManager lm = new LinearLayoutManager(this);
+        rv.setLayoutManager(lm);
+        rv.setAdapter(itemsAdapter);
+
+
+
+
+
     }
 }
