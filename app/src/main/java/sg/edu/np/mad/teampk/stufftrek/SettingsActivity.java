8c9314129e77a27d487c3daee1b2ca77cb240061
Dong En Chua <s10202623@connect.np.edu.sg>
2021-07-23 19:08:27 +0800

    Backup and load backup function

--- app/src/main/java/sg/edu/np/mad/teampk/stufftrek/SettingsActivity.java
+++ app/src/main/java/sg/edu/np/mad/teampk/stufftrek/SettingsActivity.java
@@ -4,16 +4,34 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    StorageReference storageRef;
+    FirebaseStorage storage;
+    StorageReference backupRef;
+    //Auth
+    private FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+    //Button
     Button signOutBtn;
     Button backupBtn;
     Button loadBackupBtn;
@@ -22,7 +40,14 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        //Auth
+        mAuth=FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        //Firebase cloud storage
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+        backupRef = storageRef.child("user/"+currentUser.getUid()+"/backup.db");
+        //buttons
         signOutBtn = findViewById(R.id.signOutBtn);
         backupBtn=findViewById(R.id.backupBtn);
         loadBackupBtn=findViewById(R.id.loadBackupBtn);
@@ -37,13 +62,41 @@ public class SettingsActivity extends AppCompatActivity {
         backupBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View V)
             {
+                Uri file = Uri.fromFile(new File("/data/data/sg.edu.np.mad.teampk.stufftrek/databases/stufftrekDB.db"));
+                UploadTask uploadTask = backupRef.putFile(file);
 
+                // Register observers to listen for when the download is done or if it fails
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        System.out.println("Firebase backup fail");
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                        System.out.println("Firebase backup success");
+                    }
+                });
             }
         });
         loadBackupBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View V)
             {
+                File localFile = new File("/data/data/sg.edu.np.mad.teampk.stufftrek/databases/stufftrekDB.db");
 
+
+                backupRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        System.out.println("Firebase load backup success");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        System.out.println("Firebase load backup fail");
+                    }
+                });
             }
         });
     }
