96f854b8b854adeffc057e875cbfa37166b66315
JordanChoi <s10208161@connect.np.edu.sg>
2021-07-23 08:14:11 +0800

    IMPLEMENTED IMAGGA API FOR CreateItemActivity!!!!!!!!!!!

--- app/src/main/java/sg/edu/np/mad/teampk/stufftrek/CreateItemActivity.java
+++ app/src/main/java/sg/edu/np/mad/teampk/stufftrek/CreateItemActivity.java
@@ -1,6 +1,7 @@
 package sg.edu.np.mad.teampk.stufftrek;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,8 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -32,9 +35,22 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 public class CreateItemActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
@@ -253,6 +269,8 @@ public class CreateItemActivity extends AppCompatActivity implements AdapterView
                         Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
                         picturePath = saveImage(selectedImage);
                         cameraBtn.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                        PostImageToImaggaAsync postImageToImaggaAsync = new PostImageToImaggaAsync(picturePath);
+                        postImageToImaggaAsync.execute();
                     }
                     break;
                 case 1:
@@ -308,5 +326,111 @@ public class CreateItemActivity extends AppCompatActivity implements AdapterView
         }
 
         return mypath.getAbsolutePath();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public String postImageToImagga(String filepath) throws Exception {
+        String credentialsToEncode = "acc_e1ed1ff35bf92a5" + ":" + "3b047e6e431794b65fc01306cfdf7c72";
+        String basicAuth = Base64.getEncoder().encodeToString(credentialsToEncode.getBytes(StandardCharsets.UTF_8));
+
+        File fileToUpload = new File(filepath);
+
+        String endpoint = "/tags";
+
+        String crlf = "\r\n";
+        String twoHyphens = "--";
+        String boundary =  "Image Upload";
+
+        URL urlObject = new URL("https://api.imagga.com/v2" + endpoint);
+        HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
+        connection.setRequestProperty("Authorization", "Basic " + basicAuth);
+        connection.setUseCaches(false);
+        connection.setDoOutput(true);
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Connection", "Keep-Alive");
+        connection.setRequestProperty("Cache-Control", "no-cache");
+        connection.setRequestProperty(
+                "Content-Type", "multipart/form-data;boundary=" + boundary);
+
+        DataOutputStream request = new DataOutputStream(connection.getOutputStream());
+
+        request.writeBytes(twoHyphens + boundary + crlf);
+        request.writeBytes("Content-Disposition: form-data; name=\"image\";filename=\"" + fileToUpload.getName() + "\"" + crlf);
+        request.writeBytes(crlf);
+
+
+        InputStream inputStream = new FileInputStream(fileToUpload);
+        int bytesRead;
+        byte[] dataBuffer = new byte[1024];
+        while ((bytesRead = inputStream.read(dataBuffer)) != -1) {
+            request.write(dataBuffer, 0, bytesRead);
+        }
+
+        request.writeBytes(crlf);
+        request.writeBytes(twoHyphens + boundary + twoHyphens + crlf);
+        request.flush();
+        request.close();
+
+        InputStream responseStream = new BufferedInputStream(connection.getInputStream());
+
+        BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
+
+        String line = "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while ((line = responseStreamReader.readLine()) != null) {
+            stringBuilder.append(line).append("\n");
+        }
+        responseStreamReader.close();
+
+        String response = stringBuilder.toString();
+
+        responseStream.close();
+        connection.disconnect();
+        return response;
+    }
+
+    public class PostImageToImaggaAsync extends AsyncTask<Void, Void, Void> {
+        String filePath;
+        String response;
+        String bestMatch;
+
+        public PostImageToImaggaAsync(String filePath) {
+            this.filePath = filePath;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                response = postImageToImagga(filePath);
+                JSONObject jsonObj = new JSONObject(response);
+                JSONArray tagsArr = jsonObj.getJSONObject("result").getJSONArray("tags");
+                JSONObject firstMatch = tagsArr.getJSONObject(0);
+                bestMatch = firstMatch.getJSONObject("tag").getString("en");
+                Log.i("imagga", response);
+            }
+            catch (Exception e) {
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            if (bestMatch != "")
+            {
+                bestMatch = bestMatch.substring(0,1).toUpperCase() + bestMatch.substring(1);
+            }
+
+            inputItemName.setText(bestMatch);
+        }
     }
 }
